e0d02eee5e60bd3341fdb5e7964aa0d157510d4f
Waldemar Maldonado <wmsoftwarefactory@gmail.com>
2019-09-17 14:25:31 -0400

    inicio do versionamento

---
+++ js/table.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#dtBasicExample').dataTable( {
+        paging: false,
+        searching: false
+    } );
+
+    // $('#dtBasicExample').DataTable();
+    // $('.dataTables_length').addClass('bs-select');
+
+     $('.dataTables_length').addClass('bs-select');
+
+});
